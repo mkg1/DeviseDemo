da9d45047284acd3fd9adbf6a0d2fb4c07a6ec93
mkg1 <gehring.mg@gmail.com>
2016-05-15 21:25:25 -0400

    Add delayed job

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,19 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'host name goes here...' }
+
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => 'postmaster@' +ENV["MAILGUN_SANDBOX_DOMAIN"],
+      :password       => ENV["MAILGUN_SANDBOX_PASSWORD"],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+  }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
